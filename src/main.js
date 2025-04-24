1a097f9bc58036ca73ddd2b1c0586e851bb21647
mschachinger <mschachinger@student.tgm.ac.at>
2025-04-24 21:04:03 +0200

    impoved mobile view, added scoll animations, minor fixes

--- src/main.js
+++ src/main.js
@@ -81,6 +81,28 @@ function applyHoverEffect(selector) {
 
    });
 
+
+/*
+* Scroll Fade In Animation
+*/
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.fade-section').forEach(section => {
+  gsap.from(section, {
+    opacity: 0,
+    y: 50,
+    duration: 1.2,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: section,
+      start: "top 80%",
+      toggleActions: "play none none reverse"
+    }
+  });
+});
+
+
+
 /** 
  * Mobile-Menubar
  */
